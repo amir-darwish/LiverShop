442e3b261d37a325d1373fd0fb00166dc2ca206e
Amir <mohamed.amir.darwish@gmail.com>
2024-10-03 09:30:35 +0200

    Afficher les livres via l'API par JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+// جلب البيانات من API
+fetch('https://filrouge.uha4point0.fr/V2/livres/livres') // استبدل الرابط برابط الـ API الخاص بك
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('book-container');
+
+    data.forEach(book => {
+      // إنشاء قالب HTML لكل كتاب
+      const bookElement = document.createElement('div');
+      bookElement.classList.add('box');
+
+        bookElement.innerHTML = `
+        <div class="icons">
+          <a href="#" class="fas fa-search"></a>
+          <a href="#" class="fas fa-heart"></a>
+          <a href="#" class="fas fa-eye"></a>
+        </div>
+        <div class="image">
+          <img src="imgID/${book.id}.jpg" alt="">
+        </div>
+        <div class="content">
+          <h3>${book.titre}</h3>
+          <div class="price">${book.prix} €</div>
+          <a href="#" class="btn">Ajouter au panier</a>
+        </div>
+      `;
+
+      // إضافة الكتاب إلى الـ container
+      container.appendChild(bookElement);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+
+
+
+
+
+
+
+
+
 // searchFrom = document.querySelector('.search-form');
 
 // document.querySelector('#search-btn').onclick = () => {
